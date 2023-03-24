ae589f1d90307418f6a065b53472188b141344e7
Jens Roland <jr@deepquantum.com>
2023-03-24 22:12:55 +0100

    feat: auth requirement syntax in the decorator

--- template/{{project_name}}/src/api/resources/hello/any.py
+++ template/{{project_name}}/src/api/resources/hello/any.py
@@ -1,23 +1,24 @@
 import proust
 
-# Structured logging fields: https://awslabs.github.io/aws-lambda-powertools-python/2.9.0/core/logger/#standard-structured-keys
 logger = proust.Logger()
 
 api = proust.Resource("/hello")
 
 @api.get()
 def hello_world():
-    return {"message": "Hello, world!"}
+    return {"message": "Hello, World!"}
 
 @api.get("/:name")
 def greet(params):
     name = params.get("name")
+    logger.info(f"Greeting {name} from /hello")
     return {"message": f"Greetings, {name}!"}
 
-@api.post("/:name", auth=True)
-def greet_with_data(params, data, authorized_user):
-    if not authorized_user["custom:is_moderator"]:
-        raise proust.ApiError("You are not a moderator!", 403)
+@api.post(
+    path="/:name",
+    auth=proust.Auth.require("custom:is_moderator").to_be("true")
+)
+def greet_with_data(params, data, claims):
     name = params.get("name")
     msg = data.get("msg", "[n/a]")
     return {"message": f"Ahoy-hoy, {name}! A message for you: {msg}", }
